d5f0c364a54296b36a1746c7ec2fa09c025d2217
Timur <timurka.zavyalov@mail.ru>
2021-09-22 20:59:21 +0300

    added

--- add_and_remove.py
+++ add_and_remove.py
@@ -1,12 +1,20 @@
 
 class AddRemove:
     name_county = {"Бразилия", "Бельгия", "	Германия", "Индия"}
+    add_country = str(input("Введите любую страну\n>>> "))
+    remove_country = str(input("Удалите любую страну\n>>> "))
 
-    def __init__(self, name_county):
-        self.name_county = name_county
+    # def __init__(self, name_county, add_country, remove_country):
+    #     self.name_county = name_county
+    #     self.add_country = add_country
+    #     self.remove_country = remove_country
 
     def add(self):
-        add_country = str(input("Введите любую страну"))
-        self.name_county.append(add_country)
+        return self.name_county.add(self.add_country)
+
+    def remove(self):
+        return self.name_county.remove(self.remove_country)
 
 
+addremove = AddRemove()
+addremove.add()
