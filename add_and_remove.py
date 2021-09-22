ec11c6e0144eceb600fb8d7eaf886c64c3079dfb
Timur <timurka.zavyalov@mail.ru>
2021-09-22 19:34:58 +0300

    Add

--- add_and_remove.py
+++ add_and_remove.py
@@ -1,2 +1,11 @@
+
 class AddRemove:
-    pass
+    name_county = {"Бразилия", "Бельгия", "	Германия", "Индия"}
+
+    def __init__(self, name_county):
+        self.name_county = name_county
+
+    def add(self):
+        add_country = str(input("Введите любую страну"))
+        
+
